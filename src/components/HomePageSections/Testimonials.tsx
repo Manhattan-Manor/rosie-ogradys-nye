fca1abcded8e4c94edd0e2df9b9cc0654892f9c1
Axel León <aleon@cnt.lat>
2024-09-04 09:14:58 -0600

    Removed position

--- src/components/HomePageSections/Testimonials.tsx
+++ src/components/HomePageSections/Testimonials.tsx
@@ -42,7 +42,7 @@ const Testimonials: FC = () => {
             <p className="testimonial">
               {testimonial.testimonial}
               <span>{testimonial.name}</span>
-              {testimonial.position && <span>{testimonial.position}</span>}
+              {/* {testimonial.position && <span>{testimonial.position}</span>} */}
             </p>
           </div>
         ))}
